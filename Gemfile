27675e1d7795f10ea8f725593e39209bfc53235f
aadams22 <ashleigh.adams22@gmail.com>
2016-05-19 13:35:09 -0400

    adding readme

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 gem 'http'
 gem 'json'
-gem 'faye-websocket'
-gem 'eventmachine'
+gem 'faye-websocket', '~> 0.10.3'
+gem 'eventmachine', '~> 1.2', '>= 1.2.0.1'
--- shield.rb
+++ shield.rb
@@ -1,12 +1,32 @@
 require 'http'
 require 'json'
-require 'faye-websocket'
+require 'faye/websocket'
 require 'eventmachine'
 
-rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
-  token: ENV['SHIELD_SLACKBOT_KEY'],
-  channel: '#general',
-  text: 'hello from me',
-  as_user: true
+
+rc = HTTP.post("https://slack.com/api/rtm.start", params: {
+  token: ENV['SHIELD_SLACKBOT_KEY']
   })
-puts JSON.pretty_generate(JSON.parse(rc.body))
+
+rc = JSON.parse(rc.body)
+
+url = rc['url']
+
+EM.run do
+  ws = Faye::WebSocket::Client.new(url)
+
+  ws.on :open do
+    p [:open]
+  end
+
+  ws.on :message do |event|
+    p [:message, JSON.parse(event.data)]
+  end
+
+  ws.on :close do |event|
+    p [:close, event.code, event.rc]
+    ws = nil
+    EM.stop
+  end
+
+end
